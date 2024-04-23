f8f4b9458bf152e9695bfbed7e8718dcaa7f8f5e
DilshanIBY <dilshaniru@gmail.com>
2024-04-23 17:48:32 +0530

    a simple code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # main.py
+import utils, constants
 
 def main():
     print("Hello, GitHub! Welcome to version control.")
-    result = multiply(5, 3)
+    result = utils.multiply(5, 3)
     print("Result of multiplication:", result)
 
 if __name__ == "__main__":
